e5dfdfdb03fb617403e1e00e4574ecbdc57af1dd
Alex <alex.ayllon@sage.com>
2026-03-26 14:06:24 +0100

    add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.cuantocuestaunpiso.es",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+  ];
+}
